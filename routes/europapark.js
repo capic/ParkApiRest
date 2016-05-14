8a94665790f783338ac69cecf424c35c1fd6dd5f
Vincent <vcapic@gmail.com>
2016-05-14 16:12:30 +0200

    ajout de config

--- routes/europapark.js
+++ routes/europapark.js
@@ -2,20 +2,26 @@ var express = require('express');
 var router = express.Router();
 var wdwjs = require('wdwjs');
 
-/* GET users listing. */
-router.get('/waitingtimes', function(req, res, next) {
-    var eupopaparkObject = new wdwjs.EuropaPark(
-        {debug: true,
+var eupopaparkObject = new wdwjs.EuropaPark(
+    {debug: true,
         timeFormat: "YYYY-MM-DDTHH:mm:ssZ",
         dateFormat: "YYYY-MM-DD",
         timeFormatTimezone: "Europe/London",
         scheduleMaxDates: 7}
-    );
+);
 
+router.get('/waitingtimes', function(req, res, next) {
+    eupopaparkObject.GetWaitTimes(function(err, data) {
+        if (err) return console.error("Error fetching Magic Kingdom wait times: " + err);
+
+        res.json(data);
+    });
+});
+
+router.get('/openingtimes', function(req, res, next) {
     eupopaparkObject.GetOpeningTimes(function(err, data) {
         if (err) return console.error("Error fetching Magic Kingdom wait times: " + err);
 
-        console.log(JSON.stringify(data, null, 2));
         res.json(data);
     });
 });
